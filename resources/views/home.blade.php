7d1d66f271f27e34b87b4be32ee29e2e99e282e6
anulax1225 <anulax1225@icloud.com>
2024-09-24 08:52:42 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot name="title">Home</x-slot>
+    <x-slot name="jslinks"></x-slot>
+    <x-slot name="csslinks"></x-slot>
+    
+</x-layout>
---
+++ resources/views/prefab/item-list.blade.php
@@ -0,0 +1,4 @@
+<a href="{{ $href }}" class="flex laptop:w-1/2 my-5 mx-10">
+    <img class="laptop:w-[20px] dark:brightness-0 dark:invert" src="/img/{{ $icon }}.svg">
+    <p class="text-xl ml-2">{{ $name }}</p>
+</a>
