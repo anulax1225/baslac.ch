e5e8960cbfe6fa4e90fd2d89339d56cec1364883
anulax1225 <anulax1225@icloud.com>
2025-01-22 21:42:26 +0100

    Modif mail

--- resources/views/email/auth/reset.blade.php
+++ resources/views/email/auth/reset.blade.php
@@ -1,4 +1,4 @@
-Bonjour voici un lien pour pouvoir initialisée ton compte.
-{{ config("app.url") . "/reset-password/" .$token }}
-
-Cordialement la team Baslac
+<p>Bonjour,</p>
+<p>voici un lien pour pouvoir initialisée ton compte.</p>
+<a>{{ config("app.url") . "/reset-password/" .$token }}</a>
+<p>Cordialement la team Baslac</p>
