22f9bf1687fd02aadec1de19f0c4f701a1c70b35
anulax1225 <anulax1225@icloud.com>
2025-01-27 22:52:18 +0100

    Redesign page user admin list

--- database/migrations/2025_01_27_223312_change_profile_none.php
+++ database/migrations/2025_01_27_223312_change_profile_none.php
@@ -12,7 +12,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::table("users")->where("path", "profiles/none.svg")->update([
+        DB::table("users")->where("path", "profiles/none.png")->update([
             "path" => "profiles/none.webp"
         ]);
     }
--- resources/js/Pages/Admin/User/Index.vue
+++ resources/js/Pages/Admin/User/Index.vue
@@ -61,7 +61,7 @@ const resetPassword = (id, email) => {
                     </div>
                     <div v-for="(user, index) in users" class="w-full grid p-2 border-b laptop:text-md text-sm boder text-center items-center"
                     style="grid-template-columns: repeat(15, minmax(0, 1fr));">
-                        <div class="desktop:w-14 desktop:h-14 laptop:w-12 laptop:h-12 w-10 h-10 rounded-full overflow-hidden col-span-1 flex items-center bg-black">
+                        <div class="desktop:w-14 desktop:h-14 laptop:w-12 laptop:h-12 w-10 h-10 rounded-full overflow-hidden col-span-1 flex items-center justify-center bg-black">
                             <img :src="user.pic" :class="{ 'desktop:h-14 laptop:h-12 h-10': user.path !== 'profiles/none.webp'}">
                         </div>
                         <p class="col-span-2">{{ user.name }}</p>
