8d5908cbd718a612cec6bde13b296b9081c18568
anulax1225 <anulax1225@icloud.com>
2025-01-27 22:21:34 +0100

    Modified email reset to have a link

--- resources/views/email/auth/reset-text.blade.php
+++ resources/views/email/auth/reset-text.blade.php
@@ -1,4 +1,5 @@
-Bonjour voici un lien pour pouvoir initialisée ton compte.
+Bonjour 
+voici un lien pour pouvoir initialisée ton compte.
 {{ config("app.url") . "/reset-password/" .$token }}
 
 Cordialement la team Baslac
--- resources/views/email/auth/reset.blade.php
+++ resources/views/email/auth/reset.blade.php
@@ -1,4 +1,4 @@
 <p>Bonjour,</p>
 <p>voici un lien pour pouvoir initialisée ton compte.</p>
-<a>{{ config("app.url") . "/reset-password/" .$token }}</a>
+<a href="{{config("app.url") . "/reset-password/" .$token}}">Clique ici</a>
 <p>Cordialement la team Baslac</p>
