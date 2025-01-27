e2316468f4c8774233ea000b6efbf83cdb33b808
anulax1225 <anulax1225@icloud.com>
2025-01-27 23:02:59 +0100

    Account deletion

--- database/migrations/2025_01_27_223312_change_profile_none.php
+++ database/migrations/2025_01_27_223312_change_profile_none.php
@@ -15,6 +15,12 @@ return new class extends Migration
         DB::table("users")->where("path", "profiles/none.png")->update([
             "path" => "profiles/none.webp"
         ]);
+        $anulax = DB::table("users")->where("email", "anulax1225@icloud.com")->first();
+        $gmail = DB::table("users")->where("email", "vinayak.ambigapathy@gmail.com")->first();
+        if($anulax && $gmail){
+            DB::table("photos")->where("user_id", $anulax->id)->update([ "user_id" => $gmail->id ]);
+            DB::table("albums")->where("user_id", $anulax->id)->update([ "user_id" => $gmail->id ]);
+        }
     }
 
     /**
